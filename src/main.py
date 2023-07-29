84d20d0f29709e8f5a67f469df3468c8c9d2fcb5
ruanb <ruan@isaziconsulting.co.za>
2023-07-29 22:39:33 +0200

    Small changes

--- src/main.py
+++ src/main.py
@@ -6,15 +6,20 @@ from runner import ModelRunner
 
 def main():
     run = sys.argv[1]
+    print("Loading data")
+    print("##############################################")
     dataloader = DataWrapper("cifar", 400)
     size = 1
     for i in range(10):
+        print("Starting run with n=" + str(i))
+
         model = ResNet(3, 100, size)
         model.to(get_default_device())
         runner = ModelRunner(model, dataloader.trainloader,
                              dataloader.testloader, 150, 0.001, 0.01, 0.01)
         runner.run(4)
         size += 2
+        print("##############################################")
 
     print("##############################################")
     print("Starting resnet medium run")
