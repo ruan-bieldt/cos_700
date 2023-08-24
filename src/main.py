9b65389630461d14a3aaef62e2397a32e01685a8
ruanb <ruan@isaziconsulting.co.za>
2023-08-24 14:30:04 +0200

    Fixed bug

--- src/main.py
+++ src/main.py
@@ -13,10 +13,10 @@ def main():
     size = 1
     for i in range(5):
         print("Starting run with n=" + str(i))
-
+        name = "smallres_n"+str(i)
         model = ResNet(3, 100, size, 64)
         model.to(get_default_device())
-        runner = BaseTrainer(model, dataloader.trainloader,
+        runner = BaseTrainer(model, name, dataloader.trainloader,
                              dataloader.testloader, 150, 0.001, 0.01, 0.01)
         runner.run(3)
         size += 2
