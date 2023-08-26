c60f153585f54a06ea69f5df1ea655dcefabb5e9
ruanb <ruan@isaziconsulting.co.za>
2023-08-26 20:55:54 +0200

    added new dataset

--- src/data_handler.py
+++ src/data_handler.py
@@ -2,54 +2,149 @@ import torchvision
 import numpy as np
 import torch
 import torchvision.transforms as tt
-
-
+import imageio
+from collections import defaultdict
+from tqdm.autonotebook import tqdm
 import os
 from PIL import Image
 from torch.utils.data import Dataset
 
 
+def download_and_unzip(URL, root_dir):
+    error_message = "Download is not yet implemented. Please, go to {URL} urself."
+    raise NotImplementedError(error_message.format(URL))
+
+
+def _add_channels(img, total_channels=3):
+    while len(img.shape) < 3:  # third axis is the channels
+        img = np.expand_dims(img, axis=-1)
+    while (img.shape[-1]) < 3:
+        img = np.concatenate([img, img[:, :, -1:]], axis=-1)
+    return img
+
+
+class TinyImageNetPaths:
+    def __init__(self, root_dir, download=False):
+        if download:
+            download_and_unzip('http://cs231n.stanford.edu/tiny-imagenet-200.zip',
+                               root_dir)
+        train_path = os.path.join(root_dir, 'train')
+        val_path = os.path.join(root_dir, 'val')
+        test_path = os.path.join(root_dir, 'test')
+
+        wnids_path = os.path.join(root_dir, 'wnids.txt')
+        words_path = os.path.join(root_dir, 'words.txt')
+
+        self._make_paths(train_path, val_path, test_path,
+                         wnids_path, words_path)
+
+    def _make_paths(self, train_path, val_path, test_path,
+                    wnids_path, words_path):
+        self.ids = []
+        with open(wnids_path, 'r') as idf:
+            for nid in idf:
+                nid = nid.strip()
+                self.ids.append(nid)
+        self.nid_to_words = defaultdict(list)
+        with open(words_path, 'r') as wf:
+            for line in wf:
+                nid, labels = line.split('\t')
+                labels = list(map(lambda x: x.strip(), labels.split(',')))
+                self.nid_to_words[nid].extend(labels)
+
+        self.paths = {
+            'train': [],  # [img_path, id, nid, box]
+            'val': [],  # [img_path, id, nid, box]
+            'test': []  # img_path
+        }
+
+        # Get the test paths
+        self.paths['test'] = list(map(lambda x: os.path.join(test_path, x),
+                                      os.listdir(test_path)))
+        # Get the validation paths and labels
+        with open(os.path.join(val_path, 'val_annotations.txt')) as valf:
+            for line in valf:
+                fname, nid, x0, y0, x1, y1 = line.split()
+                fname = os.path.join(val_path, 'images', fname)
+                bbox = int(x0), int(y0), int(x1), int(y1)
+                label_id = self.ids.index(nid)
+                self.paths['val'].append((fname, label_id, nid, bbox))
+
+        # Get the training paths
+        train_nids = os.listdir(train_path)
+        for nid in train_nids:
+            anno_path = os.path.join(train_path, nid, nid+'_boxes.txt')
+            imgs_path = os.path.join(train_path, nid, 'images')
+            label_id = self.ids.index(nid)
+            with open(anno_path, 'r') as annof:
+                for line in annof:
+                    fname, x0, y0, x1, y1 = line.split()
+                    fname = os.path.join(imgs_path, fname)
+                    bbox = int(x0), int(y0), int(x1), int(y1)
+                    self.paths['train'].append((fname, label_id, nid, bbox))
+
+
 class TinyImageNetDataset(Dataset):
-    def __init__(self, root, train=False, transform=None):
-        self.root = root
-        self.train = train
+    def __init__(self, root_dir, mode='train', preload=True, load_transform=None,
+                 transform=None, download=False, max_samples=None):
+        tinp = TinyImageNetPaths(root_dir, download)
+        self.mode = mode
+        self.label_idx = 1  # from [image, id, nid, box]
+        self.preload = preload
         self.transform = transform
+        self.transform_results = dict()
 
-        if self.train:
-            self.data_folder = os.path.join(self.root, 'train')
-        else:
-            self.data_folder = os.path.join(self.root, 'val', 'images')
-            self.labels_file = os.path.join(
-                self.root, 'val', 'val_annotations.txt')
-            self.labels = self._load_labels()
+        self.IMAGE_SHAPE = (64, 64, 3)
 
-        self.image_paths = sorted(os.listdir(self.data_folder))
+        self.img_data = []
+        self.label_data = []
+
+        self.max_samples = max_samples
+        self.samples = tinp.paths[mode]
+        self.samples_num = len(self.samples)
+
+        if self.max_samples is not None:
+            self.samples_num = min(self.max_samples, self.samples_num)
+            self.samples = np.random.permutation(
+                self.samples)[:self.samples_num]
+
+        if self.preload:
+            load_desc = "Preloading {} data...".format(mode)
+            self.img_data = np.zeros((self.samples_num,) + self.IMAGE_SHAPE,
+                                     dtype=np.float32)
+            self.label_data = np.zeros((self.samples_num,), dtype=np.int)
+            for idx in tqdm(range(self.samples_num), desc=load_desc):
+                s = self.samples[idx]
+                img = imageio.imread(s[0])
+                img = _add_channels(img)
+                self.img_data[idx] = img
+                if mode != 'test':
+                    self.label_data[idx] = s[self.label_idx]
+
+            if load_transform:
+                for lt in load_transform:
+                    result = lt(self.img_data, self.label_data)
+                    self.img_data, self.label_data = result[:2]
+                    if len(result) > 2:
+                        self.transform_results.update(result[2])
 
     def __len__(self):
-        return len(self.image_paths)
+        return self.samples_num
 
     def __getitem__(self, idx):
-        image_name = self.image_paths[idx]
-        image_path = os.path.join(self.data_folder, image_name)
-        image = Image.open(image_path).convert('RGB')
-
-        if self.mode == 'val':
-            label = self.labels[image_name]
+        if self.preload:
+            img = self.img_data[idx]
+            lbl = None if self.mode == 'test' else self.label_data[idx]
         else:
-            label = image_name.split('_')[0]
+            s = self.samples[idx]
+            img = imageio.imread(s[0])
+            img = _add_channels(img)
+            lbl = None if self.mode == 'test' else s[self.label_idx]
+        sample = {'image': img, 'label': lbl}
 
-        if self.transform is not None:
-            image = self.transform(image)
-
-        return image, label
-
-    def _load_labels(self):
-        labels = {}
-        with open(self.labels_file, 'r') as f:
-            for line in f:
-                line = line.strip().split('\t')
-                labels[line[0]] = line[1]
-        return labels
+        if self.transform:
+            sample = self.transform(sample)
+        return sample
 
 
 class DataWrapper:
