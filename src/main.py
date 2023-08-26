0aa82da297c6ca265127a0e80d64244f54eb9623
ruanb <ruan@isaziconsulting.co.za>
2023-08-26 19:14:53 +0200

    changed dataset

--- src/main.py
+++ src/main.py
@@ -9,15 +9,15 @@ def main():
     mode = sys.argv[2]
     print("Loading data")
     print("##############################################")
-    dataloader = DataWrapper("cifar", 400)
+    dataloader = DataWrapper("tiny", 200)
     size = 1
     for i in range(5):
         print("Starting run with n=" + str(i))
         name = "smallres_n"+str(i)
-        model = ResNet(3, 100, size, 64)
+        model = ResNet(3, 200, size, 64)
         model.to(get_default_device())
         runner = BaseTrainer(model, name, dataloader.trainloader,
-                             dataloader.testloader, 150, 0.001, 0.01, 0.01)
+                             dataloader.testloader, 300, 0.001, 0.01, 0.01)
         runner.run(3)
         size += 2
         print("##############################################")
