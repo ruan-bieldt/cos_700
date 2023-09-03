f0b80ccca0d4a6a6adc631c562f33c5b9d279800
ruanb <ruan@isaziconsulting.co.za>
2023-09-03 18:38:53 +0200

    cpu mode for student

--- FT/train_student_svd.py
+++ FT/train_student_svd.py
@@ -189,7 +189,7 @@ def train(teacher, student, epoch):
         student_outputs = student(inputs)
 
         teacher_features = apply_svd(teacher_outputs[2], 10)
-        student_features = apply_svd(student_outputs[2], 10)
+        student_features = apply_svd(student_outputs[2].cpu(), 10)
 
         loss = BETA * (criterion(utils.FT(student_features), utils.FT(teacher_features))) \
             + criterion_CE(student_outputs[3], targets)
