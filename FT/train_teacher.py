a8c0639bf2c041c809c7770d2104607d31ee01e0
ruanb <ruan@isaziconsulting.co.za>
2023-09-22 18:27:37 +0200

    teach

--- FT/train_teacher.py
+++ FT/train_teacher.py
@@ -1,4 +1,13 @@
 
+from Models import *
+import utils
+from logger import SummaryLogger
+import random
+import torch.optim as optim
+import torch.nn as nn
+import torch
+import torchvision.transforms as transforms
+import torchvision
 import argparse
 import json
 import time
@@ -6,16 +15,6 @@ from datetime import datetime
 import warnings
 import os
 warnings.filterwarnings("ignore")
-
-import torchvision
-import torchvision.transforms as transforms
-import torch
-import torch.nn as nn
-import torch.optim as optim
-import random
-from logger import SummaryLogger
-import utils
-from Models import *
 
 
 parser = argparse.ArgumentParser(description='Factor transfer for CIFAR10')
@@ -50,7 +49,7 @@ torch.manual_seed(num)
 os.environ['CUDA_VISIBLE_DEVICES'] = args.cu_num
 
 
-#Data loader
+# Data loader
 transform_train = transforms.Compose([
     transforms.RandomCrop(32, padding=4),
     transforms.RandomHorizontalFlip(),
@@ -63,16 +62,19 @@ transform_test = transforms.Compose([
     transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
 ])
 
-trainset = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform_train)
-trainloader = torch.utils.data.DataLoader(trainset, batch_size=128, shuffle=True, num_workers=4)
+trainset = torchvision.datasets.CIFAR10(
+    root='./data', train=True, download=True, transform=transform_train)
+trainloader = torch.utils.data.DataLoader(
+    trainset, batch_size=128, shuffle=True, num_workers=4)
 
-testset = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=transform_test)
-testloader = torch.utils.data.DataLoader(testset, batch_size=100, shuffle=False, num_workers=4)
+testset = torchvision.datasets.CIFAR10(
+    root='./data', train=False, download=True, transform=transform_test)
+testloader = torch.utils.data.DataLoader(
+    testset, batch_size=100, shuffle=False, num_workers=4)
 
 
-
-#Other parameters
-DEVICE = torch.device("cuda")
+# Other parameters
+DEVICE = torch.device("mps")
 RESUME_EPOCH = args.resume_epoch
 DECAY_EPOCH = args.decay_epoch
 DECAY_EPOCH = [ep - RESUME_EPOCH for ep in DECAY_EPOCH]
@@ -82,15 +84,17 @@ W_DECAY = args.w_decay
 base_lr = args.lr
 
 # Model
-model = ResNet56()
-#model = ResNet20()
+# model = ResNet56()
+model = ResNet20()
 
 
 model.to(DEVICE)
 
 # Loss and Optimizer
-optimizer = optim.SGD(model.parameters(), lr=base_lr, momentum=0.9, weight_decay=W_DECAY)
-scheduler = optim.lr_scheduler.MultiStepLR(optimizer, milestones=DECAY_EPOCH, gamma=0.1)
+optimizer = optim.SGD(model.parameters(), lr=base_lr,
+                      momentum=0.9, weight_decay=W_DECAY)
+scheduler = optim.lr_scheduler.MultiStepLR(
+    optimizer, milestones=DECAY_EPOCH, gamma=0.1)
 criterion_CE = nn.CrossEntropyLoss()
 
 
@@ -104,29 +108,29 @@ def eval(net):
 
     correct = 0
 
-
     total = 0
     criterion_CE = nn.CrossEntropyLoss()
 
     for batch_idx, (inputs, targets) in enumerate(loader):
         inputs, targets = inputs.to(DEVICE), targets.to(DEVICE)
-        outputs= net(inputs)
-
+        outputs = net(inputs)
 
         loss = criterion_CE(outputs[3], targets)
         val_loss += loss.item()
 
         _, predicted = torch.max(outputs[3].data, 1)
 
-
         total += targets.size(0)
 
         correct += predicted.eq(targets.data).cpu().sum().float().item()
         b_idx = batch_idx
 
-    print('%s \t Time Taken: %.2f sec' % (flag, time.time() - epoch_start_time))
-    print('Loss: %.3f | Acc net: %.3f%%' % (train_loss / (b_idx + 1), 100. * correct / total))
+    print('%s \t Time Taken: %.2f sec' %
+          (flag, time.time() - epoch_start_time))
+    print('Loss: %.3f | Acc net: %.3f%%' %
+          (train_loss / (b_idx + 1), 100. * correct / total))
     return val_loss / (b_idx + 1),  correct / total
+
 
 def train(model, epoch):
     epoch_start_time = time.time()
@@ -138,15 +142,12 @@ def train(model, epoch):
     total = 0
     global optimizer
 
-
-
     for batch_idx, (inputs, targets) in enumerate(trainloader):
         inputs, targets = inputs.to(DEVICE), targets.to(DEVICE)
         optimizer.zero_grad()
 
-
         ###################################################################################
-        outputs= model(inputs)
+        outputs = model(inputs)
         loss = criterion_CE(outputs[3], targets)
         ###################################################################################
         loss.backward()
@@ -159,19 +160,19 @@ def train(model, epoch):
 
         correct += predicted.eq(targets.data).cpu().sum().float().item()
 
-
         b_idx = batch_idx
 
-    print('Train s1 \t Time Taken: %.2f sec' % (time.time() - epoch_start_time))
-    print('Loss: %.3f | Acc net: %.3f%%|' % (train_loss / (b_idx + 1), 100. * correct / total))
+    print('Train s1 \t Time Taken: %.2f sec' %
+          (time.time() - epoch_start_time))
+    print('Loss: %.3f | Acc net: %.3f%%|' %
+          (train_loss / (b_idx + 1), 100. * correct / total))
     return train_loss / (b_idx + 1), correct / total
 
 
 if __name__ == '__main__':
     time_log = datetime.now().strftime('%m-%d %H:%M')
-    if int(args.log_time) :
+    if int(args.log_time):
         folder_name = 'teacher_{}'.format(time_log)
-
 
     path = os.path.join(EXPERIMENT_NAME, folder_name)
     if not os.path.exists('ckpt/' + path):
@@ -185,7 +186,6 @@ if __name__ == '__main__':
     # Instantiate logger
     logger = SummaryLogger(path)
 
-
     for epoch in range(RESUME_EPOCH, FINAL_EPOCH+1):
         f = open(os.path.join("logs/" + path, 'log.txt'), "a")
 
@@ -193,13 +193,12 @@ if __name__ == '__main__':
         train_loss, acc = train(model, epoch)
         scheduler.step()
 
-
         ### Evaluate  ###
-        val_loss, test_acc  = eval(model)
-
+        val_loss, test_acc = eval(model)
 
         f.write('EPOCH {epoch} \t'
-                'ACC_net : {acc_net:.4f} \t  \n'.format(epoch=epoch, acc_net=test_acc)
+                'ACC_net : {acc_net:.4f} \t  \n'.format(
+                    epoch=epoch, acc_net=test_acc)
                 )
         f.close()
 
@@ -208,4 +207,3 @@ if __name__ == '__main__':
         'state_dict': model.state_dict(),
         'optimizer': optimizer.state_dict(),
     }, True, 'ckpt/' + path, filename='Model_{}.pth'.format(epoch))
-
